cf9fb148586388ee8a94c9eb383c2bf88e52128a
Jimmy Shah <jimmyshah83@gmail.com>
2020-07-09 21:43:36 -0400

    Initial commit

---
+++ src/main/java/io/pivotal/pal/k8s/PalK8sApplication.java
@@ -0,0 +1,12 @@
+package io.pivotal.pal.k8s;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PalK8sApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PalK8sApplication.class, args);
+    }
+}
