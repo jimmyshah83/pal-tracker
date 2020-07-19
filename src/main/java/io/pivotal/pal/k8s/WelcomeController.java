4e9151e6f67271500efd6325f70635e73d42d6f2
Jimmy Shah <jimmyshah83@gmail.com>
2020-07-19 15:51:59 -0400

    Adding config maps to k8s deployment

--- src/main/java/io/pivotal/pal/k8s/WelcomeController.java
+++ src/main/java/io/pivotal/pal/k8s/WelcomeController.java
@@ -1,13 +1,19 @@
 package io.pivotal.pal.k8s;
 
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Data
 @RestController
 public class WelcomeController {
 
+    @Value("${welcome.message}")
+    private String message;
+
     @GetMapping("/")
     public String sayHello() {
-        return "hello";
+        return message;
     }
 }
